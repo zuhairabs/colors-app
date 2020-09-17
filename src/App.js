c252bdd4942338efafe751518ce8e044510397f1
zuhairabs <zuhairabs@gmail.com>
2020-09-18 01:47:16 +0530

    Colors App Add More Links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Palette from './Palatte';
+import SingleColorPalette from './SingleColorPalette';
 import PaletteList from './PalatteList';
 import seedColors from './seedColors';
 import {generatePalette} from './colorHelpers';
@@ -15,9 +16,10 @@ class App extends Component {
   render(){
     return (
       <Switch>
-        <Route exact path="/" render={() => <PaletteList palettes={seedColors} />} />
+        <Route exact path="/" render={(routeProps) => <PaletteList palettes={seedColors} {...routeProps} />} />
         <Route exact path="/palette/:id" render={(routeProps) => <Palette palette={generatePalette(
         this.findPalette(routeProps.match.params.id))} />} />
+        <Route exact path="/palette/:paletteId/:colorId" render={() => <SingleColorPalette />} />
       </Switch>
   );
   }
--- src/PalatteList.js
+++ src/PalatteList.js
@@ -1,18 +1,65 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { withStyles } from '@material-ui/styles';
+import MiniPalette from './MiniPalette';
+
+const styles = {
+  root: {
+    backgroundColor: "blue",
+    height: "100vh",
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "center"
+  },
+  container: {
+    width: "50%",
+    display: "flex",
+    alignItems: "flex-start",
+    flexDirection: "column",
+    flexWrap: "wrap"
+  },
+  nav: {
+    width: "100%",
+    display: "flex",
+    justifyContent: "space-between",
+    color: "white"
+  },
+  palettes: {
+    boxSizing: "border-box",
+    width: "100%",
+    display: "grid",
+     gridTemplateColumns: "repeat(3, 30%)",
+    gridGap: "5%"
+  }
+}
 
 class PaletteList extends Component {
+  constructor(props){
+    super(props);
+    this.goToPalette = this.goToPalette.bind(this);
+  }
+  goToPalette(id){
+      this.props.history.push(`/palette/${id}`);
+    };
+  
   render(){
-    const { palettes } = this.props;
+    const { palettes, classes } = this.props;
+    
     return(
-      <div>
-      <h1>React Colors</h1>
-      {palettes.map(palette => (
-        <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
-      ))}
+      <div className={classes.root}>
+        <div className={classes.container}>
+          <nav className={classes.nav}>
+            <h1>React Colors</h1>
+          </nav>
+          <div className={classes.palettes}>
+            {palettes.map(palette => (
+             <MiniPalette {...palette} handleClick={() => this.goToPalette(palette.id)} />
+            ))}
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default PaletteList;
+export default withStyles(styles)(PaletteList);
